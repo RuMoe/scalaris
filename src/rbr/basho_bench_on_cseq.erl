d4df36a0b15f9bc30518f4c5a4c8f53e611e6d63
Jan Skrzypczak <skrzypczak@zib.de>
2018-01-29 12:14:29 +0100

    quick & dirty fast write mechanism

--- src/rbr/basho_bench_on_cseq.erl
+++ src/rbr/basho_bench_on_cseq.erl
@@ -22,6 +22,11 @@
 
 -define(TRACE(X,Y), ok).
 
+%% assumes that all writes are done for the same key
+-define(FAST_WRITES, true).
+-define(ETS_TABLE_NAME, fast_round_table).
+-define(ROUND_KEY, fast_round).
+
 -include("scalaris.hrl").
 -include("client_types.hrl").
 
@@ -37,6 +42,7 @@
 
 %% write filters
 -export([wf_add/3]).
+
 
 %% %%%%%%%%%%%%%%%%%%%%%%
 %% API for basho bench
@@ -68,33 +74,79 @@ read(Key) ->
     end.
 
 -spec write(client_key(), client_value()) -> {ok}.
-write(Key, _Val) ->
+write(Key, Val) ->
+    case ?FAST_WRITES of
+        true ->
+            fast_write(Key, Val);
+        false ->
+            {RF, CC, WF} = get_write_op_addition(),
+
+            rbrcseq:qwrite(kv_db, self(), ?RT:hash_key(Key), ?MODULE,
+                           RF, CC, WF, 1),
+            trace_mpath:thread_yield(),
+            receive
+                ?SCALARIS_RECV({qwrite_done, _ReqId, _NextFastWriteRound, _Value, _WriteRet}, {ok}); %%;
+                ?SCALARIS_RECV({qwrite_deny, _ReqId, _NextFastWriteRound, _Value, Reason},
+                               begin log:log("Write failed on key ~p: ~p~n", [Key, Reason]),
+                               {ok} end) %% TODO: extend write_result type {fail, Reason} )
+            after 5000 ->
+                    log:log("~p write hangs at key ~p, ~p~n",
+                            [self(), Key, erlang:process_info(self(), messages)]),
+                    receive
+                        ?SCALARIS_RECV({qwrite_done, _ReqId, _NextFastWriteRound, _Value, _WriteRet},
+                                       begin
+                                           log:log("~p write was only slow at key ~p~n",
+                                                   [self(), Key]),
+                                           {ok}
+                                       end); %%;
+                        ?SCALARIS_RECV({qwrite_deny, _ReqId, _NextFastWriteRound, _Value, Reason},
+                                       begin log:log("~p Write failed: ~p~n",
+                                                     [self(), Reason]),
+                                             {ok} end)
+                    end
+            end
+    end.
+
+-spec fast_write(client_key(), client_value()) -> {ok}.
+fast_write(Key, _Val) ->
+    case ets:info(?ETS_TABLE_NAME) of %% better?
+        undefined ->
+            ets:new(?ETS_TABLE_NAME, [named_table]),
+            ets:insert(?ETS_TABLE_NAME, {?ROUND_KEY, pr:new(1,1)});
+        _ -> ok
+    end,
+
+    {?ROUND_KEY, LastFastWriteRound} = hd(ets:lookup(?ETS_TABLE_NAME, ?ROUND_KEY)),
     {RF, CC, WF} = get_write_op_addition(),
 
-    rbrcseq:qwrite(kv_db, self(), ?RT:hash_key(Key), ?MODULE,
-                   RF, CC, WF, 1),
+    rbrcseq:qwrite_fast(kv_db, self(), ?RT:hash_key(Key), ?MODULE,
+                   RF, CC, WF, 1, LastFastWriteRound, 0),
     trace_mpath:thread_yield(),
     receive
-        ?SCALARIS_RECV({qwrite_done, _ReqId, _NextFastWriteRound, _Value, _WriteRet}, {ok}); %%;
-        ?SCALARIS_RECV({qwrite_deny, _ReqId, _NextFastWriteRound, _Value, Reason},
+        ?SCALARIS_RECV({qwrite_done, _ReqId, NextFastWriteRound, _Value, _WriteRet}, {ok}); %%;
+        ?SCALARIS_RECV({qwrite_deny, _ReqId, NextFastWriteRound, _Value, Reason},
                        begin log:log("Write failed on key ~p: ~p~n", [Key, Reason]),
                        {ok} end) %% TODO: extend write_result type {fail, Reason} )
     after 5000 ->
             log:log("~p write hangs at key ~p, ~p~n",
                     [self(), Key, erlang:process_info(self(), messages)]),
             receive
-                ?SCALARIS_RECV({qwrite_done, _ReqId, _NextFastWriteRound, _Value, _WriteRet},
+                ?SCALARIS_RECV({qwrite_done, _ReqId, NextFastWriteRound, _Value, _WriteRet},
                                begin
                                    log:log("~p write was only slow at key ~p~n",
                                            [self(), Key]),
                                    {ok}
                                end); %%;
-                ?SCALARIS_RECV({qwrite_deny, _ReqId, _NextFastWriteRound, _Value, Reason},
+                ?SCALARIS_RECV({qwrite_deny, _ReqId, NextFastWriteRound, _Value, Reason},
                                begin log:log("~p Write failed: ~p~n",
                                              [self(), Reason]),
                                      {ok} end)
-                end
-    end.
+            end
+    end,
+
+    ets:insert(?ETS_TABLE_NAME, {?ROUND_KEY, NextFastWriteRound}),
+    {ok}.
+
 
 %% ------------------------------- READ OPERATIONS -----------------------------
 %% @doc Reads full value.
