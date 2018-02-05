ed7731d5669b687e767fd0a6eb3f1666ca21faf0
Jan Skrzypczak <skrzypczak@zib.de>
2018-02-05 15:46:26 +0100

    dirty hack to 'kill' acceptors each minute

--- src/rbr/prbr.erl
+++ src/rbr/prbr.erl
@@ -148,20 +148,28 @@ noop_write_filter(_, UI, X) -> {X, UI}.
 
 %% initialize: return initial state.
 -spec init(atom() | tuple()) -> state().
-init(DBName) -> ?PDB:new(DBName).
+init(DBName) ->
+    {mintime(), ?PDB:new(DBName)}.
+
+mintime() -> erlang:system_time() / 1.0e9 / 60.
+reply(Key, {InitTime, _}) ->
+    hd(integer_to_list(Key)) > hd(integer_to_list(trunc(mintime() - InitTime))).
 
 %% @doc Closes the given DB (it may be recoverable using open/1 depending on
 %%      the DB back-end).
 -spec close(state()) -> true.
-close(State) -> ?PDB:close(State).
+close({_,State}) -> ?PDB:close(State).
 
 %% @doc Closes the given DB and deletes all contents (this DB can thus not be
 %%      re-opened using open/1).
 -spec close_and_delete(state()) -> true.
-close_and_delete(State) -> ?PDB:close_and_delete(State).
+close_and_delete({_, State}) -> ?PDB:close_and_delete(State).
 
 -spec on(message(), state()) -> state().
 on({prbr, round_request, _DB, Cons, Proposer, Key, DataType, ProposerUID, ReadFilter, OpType}, TableName) ->
+    case not reply(Key, TableName) of
+        true -> TableName;
+        _ ->
     ?TRACE("prbr:round_request: ~p in round ~p~n", [Key, ProposerUID]),
     KeyEntry = get_entry(Key, TableName),
 
@@ -210,10 +218,14 @@ on({prbr, round_request, _DB, Cons, Proposer, Key, DataType, ProposerUID, ReadFi
             set_entry(NewKeyEntry2, TableName)
     end,
 
-    TableName;
+    TableName
+    end;
 
 on({prbr, read, _DB, Cons, Proposer, Key, DataType, ProposerUID, ReadFilter,
-   ReadRound}, TableName) ->
+   ReadRound},  TableName) ->
+    case not reply(Key, TableName) of
+        true -> TableName;
+        _ ->
     ?TRACE("prbr:read: ~p in round ~p~n", [Key, ReadRound]),
     KeyEntry = get_entry(Key, TableName),
 
@@ -245,10 +257,14 @@ on({prbr, read, _DB, Cons, Proposer, Key, DataType, ProposerUID, ReadFilter,
          _ ->
             msg_read_deny(Proposer, Cons, ReadRound, entry_r_read(KeyEntry))
     end,
-    TableName;
+    TableName
+    end;
 
 on({prbr, write, DB, Cons, Proposer, Key, DataType, ProposerUID, InRound, OldWriteRound, Value,
     PassedToUpdate, WriteFilter, IsWriteThrough}, TableName) ->
+    case not reply(Key, TableName) of
+        true -> TableName;
+        _ ->
     ?TRACE("prbr:write for key: ~p in round ~p~n", [Key, InRound]),
     trace_mpath:log_info(self(), {prbr_on_write}),
     KeyEntry = get_entry(Key, TableName),
@@ -332,7 +348,8 @@ on({prbr, write, DB, Cons, Proposer, Key, DataType, ProposerUID, InRound, OldWri
                 %% write through attempts based on its partially completed write.
                 [msg_write_deny(P, Cons, Key, RoundTried) || P <- LearnerToNotify]
         end,
-    TableName;
+    TableName
+    end;
 
 on({prbr, init_repair_on_write, DB, Key, KnownWriteRound}, TableName) ->
     %% Triggers a repair process for this replica.
@@ -385,26 +402,26 @@ on({prbr, tab2list_raw, DB, Client}, TableName) ->
     TableName.
 
 -spec get_entry(any(), state()) -> entry().
-get_entry(Id, TableName) ->
+get_entry(Id, {_, TableName}) ->
     case ?PDB:get(TableName, Id) of
         {}    -> new(Id);
         Entry -> Entry
     end.
 
 -spec set_entry(entry(), state()) -> state().
-set_entry(NewEntry, TableName) ->
+set_entry(NewEntry, {_, TableName}) ->
     _ = ?PDB:set(TableName, NewEntry),
     TableName.
 
 -spec delete_entry(state(), entry()) -> db_prbr:db().
-delete_entry(TableName, Entry) ->
+delete_entry({_, TableName}, Entry) ->
     ?PDB:delete_entry(TableName, Entry).
 
 -spec get_load(state()) -> non_neg_integer().
-get_load(State) -> ?PDB:get_load(State).
+get_load({_, State}) -> ?PDB:get_load(State).
 
 -spec tab2list(state()) -> [{any(),any()}].
-tab2list(State) ->
+tab2list({_, State}) ->
     %% without prbr own data
     Entries = tab2list_raw(State),
     [ {entry_key(X), entry_val(X)} || X <- Entries].
